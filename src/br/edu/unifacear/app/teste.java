be47dc0d8a2740736b46c2c47202493fe0a30cf4
JhonathanPvc <37600697+JhonathanPvc@users.noreply.github.com>
2018-09-02 23:06:40 -0300

    Foi

--- src/br/edu/unifacear/app/teste.java
+++ src/br/edu/unifacear/app/teste.java
@@ -1,8 +1,5 @@
 package br.edu.unifacear.app;
 
-import br.edu.unifacear.busines.FormaPagamentoBusines;
-import br.edu.unifacear.model.FormaPagamento;
-
 public class teste {
 
 	public static void main(String[] args) {
